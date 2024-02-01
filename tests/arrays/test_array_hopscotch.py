e6079153dc28218c51a0d795fcd3fe8cae4eeb04
RichardDKlein <RichardDKlein@gmail.com>
2024-02-01 14:52:03 -0600

    Tweak test_array_hopscotch

--- tests/arrays/test_array_hopscotch.py
+++ tests/arrays/test_array_hopscotch.py
@@ -1,38 +1,88 @@
-import pytest
 from src.arrays.array_hopscotch import array_hopscotch
 
 
-@pytest.fixture(scope="session")
-def before_first_test():
+def test_empty_array():
     print()
+    print()
+    print("===================")
     print("Test ArrayHopscotch:")
     print("===================")
 
+    a = ()
+    i_start = 0
+    expected = set()
+    do_test(a, i_start, expected)
 
-@pytest.mark.parametrize(
-    "a, i_start, expected",
-    [
-        ((), 0, set()),
-        ((2, 3, 1, 0, 5), -1, set()),
-        ((2, 3, 1, 0, 5), 5, set()),
-        ((2, 3, 1, 0, 5), 0, {(0, 2, 3)}),
-        ((2, 2, 2, 0, 0), 0, {(0, 2, 4)}),
-        ((1, 1, 1, 3, 1, 2, 0, 3), 5, {(5, 3, 6), (5, 7, 4, 3, 6)}),
-        ((4, 2, 0, 3, 1, 5, 0), 0, {(0, 4, 3, 6)}),
-        ((5, 2, 1, 3, 0, 1, 2, 4, 1), 3, {(3, 0, 5, 4), (3, 0, 5, 6, 4), (3, 6, 4)}),
-        ((2, 2, 2, 0, 2), 0, set()),
-    ],
-)
-def test_array_hopscotch(before_first_test, a, i_start, expected):
+
+def test_start_index_too_small():
+    a = (2, 3, 1, 0, 5)
+    i_start = -1
+    expected = set()
+    do_test(a, i_start, expected)
+
+
+def test_start_index_too_large():
+    a = (2, 3, 1, 0, 5)
+    i_start = 5
+    expected = set()
+    do_test(a, i_start, expected)
+
+
+def test_start_index_already_wins():
+    a = (2, 3, 1, 0, 5)
+    i_start = 3
+    expected = {(3,)}
+    do_test(a, i_start, expected)
+
+
+def test_one_winning_path_case_1():
+    a = (2, 3, 1, 0, 5)
+    i_start = 0
+    expected = {(0, 2, 3)}
+    do_test(a, i_start, expected)
+
+
+def test_one_winning_path_case_2():
+    a = (2, 2, 2, 0, 0)
+    i_start = 0
+    expected = {(0, 2, 4)}
+    do_test(a, i_start, expected)
+
+
+def test_one_winning_path_case_3():
+    a = (4, 2, 0, 3, 1, 5, 0)
+    i_start = 0
+    expected = {(0, 4, 3, 6)}
+    do_test(a, i_start, expected)
+
+
+def test_two_winning_paths():
+    a = (1, 1, 1, 3, 1, 2, 0, 3)
+    i_start = 5
+    expected = {(5, 3, 6), (5, 7, 4, 3, 6)}
+    do_test(a, i_start, expected)
+
+
+def test_three_winning_paths():
+    a = (5, 2, 1, 3, 0, 1, 2, 4, 1)
+    i_start = 3
+    expected = {(3, 0, 5, 4), (3, 0, 5, 6, 4), (3, 6, 4)}
+    do_test(a, i_start, expected)
+
+
+def test_no_solution():
+    a = (2, 2, 2, 0, 2)
+    i_start = 0
+    expected = set()
+    do_test(a, i_start, expected)
+
+
+def do_test(a, i_start, expected):
     actual = array_hopscotch(a, i_start)
-    assert are_sets_equal(expected, actual)
-    print(f"{a}, start = {i_start}")
+    assert expected == actual
+    print(f"\n\n{a}, start = {i_start}")
     print("Winning hops:")
     print_paths(actual)
-
-
-def are_sets_equal(set1, set2):
-    return set1 == set2
 
 
 def print_paths(paths):
