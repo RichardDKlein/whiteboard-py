568ee53afee603baba51a6ce2985d93fe2244a38
RichardDKlein <RichardDKlein@gmail.com>
2024-02-05 11:41:03 -0600

    Tweak array_subsets

--- src/arrays/array_subsets.py
+++ src/arrays/array_subsets.py
@@ -2,7 +2,7 @@ def array_subsets(a):
     result = []
     if a is None or len(a) == 0:
         return result
-    a.sort(key=lambda x: -x)  # sort in descending order
+    a.sort(reverse=True)
     sum_a = 0
     sum_b = sum(a)
     for n in a:
@@ -11,5 +11,5 @@ def array_subsets(a):
         sum_b -= n
         if sum_a > sum_b:
             break
-    result.sort()  # sort in ascending order
+    result.sort()
     return result
