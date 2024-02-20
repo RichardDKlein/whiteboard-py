40800c4d29ce5ea4b61ab41a00f9065c3b9d33fe
RichardDKlein <RichardDKlein@gmail.com>
2024-02-19 18:57:53 -0600

    Add is_prime

---
+++ src/arrays/is_prime.py
@@ -0,0 +1,8 @@
+def is_prime(n: int) -> bool:
+    """
+    Determine whether a given positive integer is prime.
+
+    :param n: A positive integer.
+    :return: `True` if `n` is prime, `False` otherwise.
+    """
+    return True
